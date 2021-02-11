cccbc7dccfaa61190fdbfbc3752da9d003d9366a
sd-Hector <56688549+sd-Hector@users.noreply.github.com>
2021-02-11 10:12:56 +0100

    Modification du fichier, mode SMOOTH

--- TP_2.c
+++ TP_2.c
@@ -4,27 +4,32 @@
 #include <time.h>
 int main()
 {
-    printf("Launching Fan control, done by: GROUPE 6\n\r");
-	
+    int i=0;
+	int sens=0;
+	char speed_cmd[128];
+	printf("Launching SMOOTH Fan control, done by: GROUPE 6\n\r");
+	system("echo 0 > /sys/devices/platform/stm-pwm/pwm1");
+
 	while(1)
 	{
-		system("echo 150 > /sys/devices/platform/stm-pwm/pwm1");
+		while(sens==0)
+		{
+			i+=51;
+			if (i>=255) {i=255;sens=1;}
+			sprintf(speed_cmd,"echo %d > /sys/devices/platform/stm-pwm/pwm1",i);
+			system(speed_cmd);
+			printf("Value = %d \t Waiting 1 seconds...\n\r",i);
+			sleep(1);
+		}
+		while(sens==1)
+		{
+			i-=51;
+			if (i<=0) {i=0;sens=0;}
+			sprintf(speed_cmd,"echo %d > /sys/devices/platform/stm-pwm/pwm1",i);
+			system(speed_cmd);
+			sleep(1);
+		}
 		
-		printf("Waiting 3 seconds...\n\r");
-		sleep(3);
-		
-		system("echo 200 > /sys/devices/platform/stm-pwm/pwm1");
-		
-		printf("Waiting 3 seconds...\n\r");
-		sleep(3);
-		
-		system("echo 255 > /sys/devices/platform/stm-pwm/pwm1");
-		
-		printf("Waiting 4 seconds...\n\r");
-		sleep(4);
-		
-		printf("Done\n\r");
-	
 	}
 
     return 0;
